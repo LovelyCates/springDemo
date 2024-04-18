eba21319783b0413350985e1a518bd80c6c50a2d
lovelycat <zaikeshishabi@163.com>
2024-04-18 22:09:22 +0800

    Demo构建

---
+++ src/main/java/edu/hubu/Main.java
@@ -0,0 +1,11 @@
+package edu.hubu;
+
+/**
+ * @author 小喵
+ * @date ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
